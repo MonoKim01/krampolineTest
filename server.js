e82215b27961bda3284becd7f86055a664d3b086
mono.kim <85483855+MonoKim01@users.noreply.github.com>
2023-04-17 12:20:23 +0900

    Update server.js

--- server.js
+++ server.js
@@ -8,5 +8,17 @@ app.get("/", (req, res) => {
   res.send("mymy");
 });
 
+app.get("/list", (req, res) => {
+  res.send("/list");
+});
+
+app.get("/monot", (req, res) => {
+  res.send("/monot");
+});
+
+app.get("/monot/list", (req, res) => {
+  res.send("/monot/list");
+});
+
 app.listen(PORT);
 console.log("Server is running");
