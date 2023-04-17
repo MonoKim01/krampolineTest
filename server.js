3db87351d057b1751b67e02f4842e78eac8a2408
mono.kim <85483855+MonoKim01@users.noreply.github.com>
2023-04-17 14:50:40 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,18 +5,22 @@ const PORT = 8080;
 //APP
 const app = express();
 app.get("/", (req, res) => {
+  console.log(JSON.stringify(req));
   res.send("mymy");
 });
 
 app.get("/list", (req, res) => {
+  console.log(JSON.stringify(req));
   res.send("/list");
 });
 
 app.get("/monot", (req, res) => {
+  console.log(JSON.stringify(req));
   res.send("/monot");
 });
 
 app.get("/monot/list", (req, res) => {
+  console.log(JSON.stringify(req));
   res.send("/monot/list");
 });
 
